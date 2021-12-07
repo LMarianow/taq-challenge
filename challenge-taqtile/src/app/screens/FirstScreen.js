adce80ee6bbf5d3676d30b1d95f22a6ca2d44062
Lucas Mariano <semalus2011@gmail.com>
2021-12-07 18:53:42 -0300

    First steps made pastes structure and first page

---
+++ challenge-taqtile/src/app/screens/FirstScreen.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+function FirstScreen(){
+    const [characters, setCharacters] = useState([]);
+
+ return(
+     <div>Teste</div>
+ );
+
+}
+
+export default FirstScreen;
