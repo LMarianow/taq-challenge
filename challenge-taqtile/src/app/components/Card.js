8a51a90c0be62ecfbcace88dbf089a1de0cfde6f
Lucas Mariano <semalus2011@gmail.com>
2021-12-09 21:39:41 -0300

    Add modal states in card component

--- challenge-taqtile/src/app/components/Card.js
+++ challenge-taqtile/src/app/components/Card.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 import { CardContainer } from '../stylesheets/components/CardStyle';
+import { Modal } from "./modals/Modal";
 
-export const Card = (props) => (
-    <CardContainer>
-        <img src={props.character.image} alt="Character" />
-        <h2>{props.character.name}</h2>
-        <p>
-            <b>Id:</b> {props.character.id}
-        </p>
-        <p>
-            <b>Status:</b> {props.character.status}
-        </p>
-    </CardContainer>
-)
+export default function Card(props) {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+      setShowModal(prev => !prev);
+    };
+
+    return (
+    <>
+        <CardContainer onClick={openModal}>
+            <img src={props.character.image} alt="Character" />
+            <h2>{props.character.name}</h2>
+            <p>
+                <b>Id:</b> {props.character.id}
+            </p>
+            <p>
+                <b>Status:</b> {props.character.status}
+            </p>
+        </CardContainer >
+        <Modal showModal={showModal} setShowModal={setShowModal} idCharacter={props.character.id} />
+    </>
+    );
+}
--- challenge-taqtile/src/app/screens/FirstScreen.js
+++ challenge-taqtile/src/app/screens/FirstScreen.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useQuery } from "@apollo/client";
 import CHARACTER_INFO from "../querys/query-character";
-import { Card } from "../components/Card"
+import Card from "../components/Card";
 import { CardList } from '../stylesheets/components/CardListStyle';
 import toast from "react-hot-toast";
 
 function FirstScreen(){
-    const { loading, error, data } = useQuery(CHARACTER_INFO)
-    if (loading) {
-        return toast.error('Ops, houve um erro. Tente novamente em alguns minutos')
-      }
-    if (error) {
+  
+  const { loading, error, data } = useQuery(CHARACTER_INFO)
+  if (loading) {
+    return toast.error('Ops, houve um erro. Tente novamente em alguns minutos')
+  }
+  if (error) {
     return <p>an error occurred...</p>;
-    }
+  }
  return(
-    <CardList>
+  <CardList>
       {data.characters.results.map((character, index) => (
-      <Card key={character.id} character={character}/>
+      <Card key={index} character={character}/>
     ))}
   </CardList>
  );
