1b33e765efbb802d9d81d5447d75888d4f4321c7
Lucas Mariano <semalus2011@gmail.com>
2021-12-11 17:38:10 -0300

    Add change color status character in styled

--- challenge-taqtile/src/app/stylesheets/components/CardStyle.js
+++ challenge-taqtile/src/app/stylesheets/components/CardStyle.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: #95dada;
-  border: 1px solid grey;
+  background-color: #fff;
+  border: 1px solid #000;
   border-radius: 5px;
   padding: 25px;
   cursor: pointer;
@@ -14,8 +14,25 @@ export const CardContainer = styled.div`
   transition: transform .25s ease-out;
   &:hover {
     transform: scale(1.05);
-    color: green;
-    border: 1px solid green;
+  }
+  label {
+    ${(props) =>
+      props.status === "Alive" &&
+      css`
+      color: #28CC2D;
+      `}
+
+      ${(props) =>
+        props.status === "Dead" &&
+        css`
+        color: #D82E3F;
+      `}
+
+      ${(props) =>
+        props.status === "unknown" &&
+        css`
+        color: #FFE135;
+      `}
   }
   img{
     border-radius: 5px;
