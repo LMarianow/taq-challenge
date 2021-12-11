b8248dcb8c32a64084ff9c91e17de71e45f31ee4
Lucas Mariano <semalus2011@gmail.com>
2021-12-11 11:54:12 -0300

    Add GlobalStyle with bg and logo to project

--- challenge-taqtile/src/App.js
+++ challenge-taqtile/src/App.js
@@ -1,8 +1,13 @@
 import FirstScreen from './app/screens/FirstScreen';
-
+import { GlobalStyle } from './app/stylesheets/components/GlobalStyle';
+import logo from '../src/app/images/ricklogo.png'
 function App() {
   return (
+    <>
+    <GlobalStyle/>
+    <img src={logo} id="logo" alt="logo" />
     <FirstScreen/>
+    </>
   );
 }
 
---
+++ challenge-taqtile/src/app/stylesheets/components/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import background from '../../images/rick-bg.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-image: url(${background});
+    background-size : contain;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    line-height: 1.3;
+    #logo {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+        margin-bottom: 2em;
+        margin-top: 2em;
+    }
+  }
+`;
